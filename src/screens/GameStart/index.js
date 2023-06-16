f440b070d5abc5fc2a7bad95b7df78c2eafe0111
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-06-15 21:42:42 -0300

    feat: Creating Custom Buttons

--- src/screens/GameStart/index.js
+++ src/screens/GameStart/index.js
@@ -1,9 +1,13 @@
+import PrimaryButton from "../../components/PrimaryButton";
+
 const { View, TextInput } = require("react-native");
 
 export default function GameStart() {
     return (
         <View>
             <TextInput />
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
         </View>
     );
 }
