c74964e2e417536723b46e2a3ef063ae37cfef18
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-06-21 21:00:54 -0300

    feat: Starting Work on the Game Screen

--- src/screens/Game/index.js
+++ src/screens/Game/index.js
@@ -1,7 +1,23 @@
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Game() {
-    return (
-        <Text>GameScreen</Text>
-    );
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      {/* GUESS */}
+      <Text>Higher or Lower?</Text>
+      {/* +
+      - */}
+      <View>
+        <Text>LOG ROUNDS</Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  }
+});
--- src/screens/GameStart/index.js
+++ src/screens/GameStart/index.js
@@ -4,71 +4,69 @@ import PrimaryButton from "../../components/PrimaryButton";
 import { View, StyleSheet, Alert } from "react-native";
 
 export default function GameStart({ onPickNumber }) {
-    const [enteredNumber, setEnteredNumber] = useState('');
+  const [enteredNumber, setEnteredNumber] = useState("");
 
-    function numberInputHandler(input) {
-        setEnteredNumber(input);
+  function numberInputHandler(input) {
+    setEnteredNumber(input);
+  }
+
+  function confirmButtonHandler() {
+    const chosenNumber = parseInt(enteredNumber);
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        "Invalid number!",
+        "Number has to be a number between 0 and 99.",
+        [{ text: "Okay", style: "destructive", onPress: resetNumberHandler }]
+      );
     }
 
-    function confirmButtonHandler() {
-        const chosenNumber = parseInt(enteredNumber)
+    onPickNumber(chosenNumber);
+  }
 
-        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
-            Alert.alert(
-                'Invalid number!', 
-                'Number has to be a number between 0 and 99.', 
-                [{ text: 'Okay', style: 'destructive', onPress:resetNumberHandler }]);
-        }
+  function resetNumberHandler() {
+    setEnteredNumber("");
+  }
 
-        onPickNumber(chosenNumber);
-    }
-
-    function resetNumberHandler() {
-        setEnteredNumber('');
-    }
-
-    return (
-        <View style={sytles.inputView}>
-            <NumberInput 
-                value={enteredNumber}
-                onChange={numberInputHandler}
-            />
-            <View style={sytles.buttonsView}>
-                <View style={sytles.buttonView}>
-                    <PrimaryButton onPress={resetNumberHandler}>Reset</PrimaryButton>
-                </View>
-                <View style={sytles.buttonView}>
-                    <PrimaryButton onPress={confirmButtonHandler}>Confirm</PrimaryButton>
-                </View>
-            </View>
+  return (
+    <View style={sytles.inputView}>
+      <NumberInput value={enteredNumber} onChange={numberInputHandler} />
+      <View style={sytles.buttonsView}>
+        <View style={sytles.buttonView}>
+          <PrimaryButton onPress={resetNumberHandler}>Reset</PrimaryButton>
         </View>
-    );
+        <View style={sytles.buttonView}>
+          <PrimaryButton onPress={confirmButtonHandler}>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const sytles = StyleSheet.create({
-    inputView: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 100,
-        padding: 16,
-        marginHorizontal: 24,
-        borderRadius: 10,
-        backgroundColor: '#4e0329',
-        // Android
-        elevation: 8,
-        // IOS
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 2
-        },
-        shadowRadius: 6,
-        shadowOpacity: 0.25,
+  inputView: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 100,
+    padding: 16,
+    marginHorizontal: 24,
+    borderRadius: 10,
+    backgroundColor: "#4e0329",
+    // Android
+    elevation: 8,
+    // IOS
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    buttonsView: {
-        flexDirection: 'row',
-    },
-    buttonView: {
-        flex: 1,
-    },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  buttonsView: {
+    flexDirection: "row",
+  },
+  buttonView: {
+    flex: 1,
+  },
 });
